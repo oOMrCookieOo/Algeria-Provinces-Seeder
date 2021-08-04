97e278a226a8bd124d6ed75690287fbfa0d73f11
oOMrCookieOo <44684334+oOMrCookieOo@users.noreply.github.com>
2021-08-04 16:46:47 +0000

    Add files via upload

---
+++ ProvincesSeeder.php
@@ -0,0 +1,539 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Province;
+use Illuminate\Database\Seeder;
+
+class ProvincesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Province::create( [
+            'id'=>1,
+            'province_id'=>1,
+            'name_ar'=>'أدرار',
+            'name_fr'=>'Adrar',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>2,
+            'province_id'=>2,
+            'name_ar'=>'الشلف',
+            'name_fr'=>'Chlef',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>3,
+            'province_id'=>3,
+            'name_ar'=>'الأغواط',
+            'name_fr'=>'Laghouat',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>4,
+            'province_id'=>4,
+            'name_ar'=>'أم البواقي',
+            'name_fr'=>'Oum El Bouaghi',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>5,
+            'province_id'=>5,
+            'name_ar'=>'باتنة',
+            'name_fr'=>'Batna',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>6,
+            'province_id'=>6,
+            'name_ar'=>'بجاية',
+            'name_fr'=>'Béjaïa',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>7,
+            'province_id'=>7,
+            'name_ar'=>'بسكرة',
+            'name_fr'=>'Biskra',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>8,
+            'province_id'=>8,
+            'name_ar'=>'بشار',
+            'name_fr'=>'Béchar',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>9,
+            'province_id'=>9,
+            'name_ar'=>'البليدة',
+            'name_fr'=>'Blida',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>10,
+            'province_id'=>10,
+            'name_ar'=>'البويرة',
+            'name_fr'=>'Bouira',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>11,
+            'province_id'=>11,
+            'name_ar'=>'تمنراست',
+            'name_fr'=>'Tamanrasset',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>12,
+            'province_id'=>12,
+            'name_ar'=>'تبسة',
+            'name_fr'=>'Tébessa',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>13,
+            'province_id'=>13,
+            'name_ar'=>'تلمسان',
+            'name_fr'=>'Tlemcen',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>14,
+            'province_id'=>14,
+            'name_ar'=>'تيارت',
+            'name_fr'=>'Tiaret',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>15,
+            'province_id'=>15,
+            'name_ar'=>'تيزي وزو',
+            'name_fr'=>'Tizi Ouzou',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>16,
+            'province_id'=>16,
+            'name_ar'=>'الجزائر',
+            'name_fr'=>'Alger',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>17,
+            'province_id'=>17,
+            'name_ar'=>'الجلفة',
+            'name_fr'=>'Djelfa',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>18,
+            'province_id'=>18,
+            'name_ar'=>'جيجل',
+            'name_fr'=>'Jijel',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>19,
+            'province_id'=>19,
+            'name_ar'=>'سطيف',
+            'name_fr'=>'Sétif',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>20,
+            'province_id'=>20,
+            'name_ar'=>'سعيدة',
+            'name_fr'=>'Saïda',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>21,
+            'province_id'=>21,
+            'name_ar'=>'سكيكدة',
+            'name_fr'=>'Skikda',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>22,
+            'province_id'=>22,
+            'name_ar'=>'سيدي بلعباس',
+            'name_fr'=>'Sidi Bel Abbès',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>23,
+            'province_id'=>23,
+            'name_ar'=>'عنابة',
+            'name_fr'=>'Annaba',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>24,
+            'province_id'=>24,
+            'name_ar'=>'قالمة',
+            'name_fr'=>'Guelma',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>25,
+            'province_id'=>25,
+            'name_ar'=>'قسنطينة',
+            'name_fr'=>'Constantine',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>26,
+            'province_id'=>26,
+            'name_ar'=>'المدية',
+            'name_fr'=>'Médéa',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>27,
+            'province_id'=>27,
+            'name_ar'=>'مستغانم',
+            'name_fr'=>'Mostaganem',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>28,
+            'province_id'=>28,
+            'name_ar'=>'المسيلة',
+            'name_fr'=>'M\'Sila',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>29,
+            'province_id'=>29,
+            'name_ar'=>'معسكر	',
+            'name_fr'=>'Mascara',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>30,
+            'province_id'=>30,
+            'name_ar'=>'ورقلة',
+            'name_fr'=>'Ouargla',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>31,
+            'province_id'=>31,
+            'name_ar'=>'وهران	',
+            'name_fr'=>'Oran',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>32,
+            'province_id'=>32,
+            'name_ar'=>'البيض',
+            'name_fr'=>'El Bayadh',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>33,
+            'province_id'=>33,
+            'name_ar'=>'اليزي',
+            'name_fr'=>'Illizi',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>34,
+            'province_id'=>34,
+            'name_ar'=>'برج بوعريريج',
+            'name_fr'=>'Bordj Bou Arreridj',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>35,
+            'province_id'=>35,
+            'name_ar'=>'بومرداس',
+            'name_fr'=>'Boumerdès',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>36,
+            'province_id'=>36,
+            'name_ar'=>'الطارف',
+            'name_fr'=>'El Tarf',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>37,
+            'province_id'=>37,
+            'name_ar'=>'تندوف',
+            'name_fr'=>'Tindouf',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>38,
+            'province_id'=>38,
+            'name_ar'=>'تسمسيلت',
+            'name_fr'=>'Tissemsilt',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>39,
+            'province_id'=>39,
+            'name_ar'=>'الوادي',
+            'name_fr'=>'El Oued',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>40,
+            'province_id'=>40,
+            'name_ar'=>'خنشلة',
+            'name_fr'=>'Khenchela',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>41,
+            'province_id'=>41,
+            'name_ar'=>'سوق أهراس',
+            'name_fr'=>'Souk Ahras',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>42,
+            'province_id'=>42,
+            'name_ar'=>'تيبازة',
+            'name_fr'=>'Tipaza',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>43,
+            'province_id'=>43,
+            'name_ar'=>'ميلة',
+            'name_fr'=>'Mila',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>44,
+            'province_id'=>44,
+            'name_ar'=>'عين الدفلى	',
+            'name_fr'=>'Aïn Defla',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>45,
+            'province_id'=>45,
+            'name_ar'=>'النعامة',
+            'name_fr'=>'Naâma',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>46,
+            'province_id'=>46,
+            'name_ar'=>'عين تموشنت',
+            'name_fr'=>'Aïn Témouchent',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>47,
+            'province_id'=>47,
+            'name_ar'=>'غرداية',
+            'name_fr'=>'Ghardaïa',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>48,
+            'province_id'=>48,
+            'name_ar'=>'غليزان',
+            'name_fr'=>'Relizane',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>49,
+            'province_id'=>49,
+            'name_ar'=>'المغير',
+            'name_fr'=>'El M\'ghair',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>50,
+            'province_id'=>50,
+            'name_ar'=>'المنيعة',
+            'name_fr'=>'El Menia',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>51,
+            'province_id'=>51,
+            'name_ar'=>'أولاد جلال',
+            'name_fr'=>'Ouled Djellal',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>52,
+            'province_id'=>52,
+            'name_ar'=>'برج باجي مختار',
+            'name_fr'=>'Bordj Baji Mokhtar',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>53,
+            'province_id'=>53,
+            'name_ar'=>'بني عباس',
+            'name_fr'=>'Béni Abbès',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>54,
+            'province_id'=>54,
+            'name_ar'=>'تيميمون',
+            'name_fr'=>'Timimoun',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>55,
+            'province_id'=>55,
+            'name_ar'=>'تقرت',
+            'name_fr'=>'Touggourt',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>56,
+            'province_id'=>56,
+            'name_ar'=>'جانت',
+            'name_fr'=>'Djanet',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>57,
+            'province_id'=>57,
+            'name_ar'=>'عين صالح',
+            'name_fr'=>'In Salah',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+
+        Province::create( [
+            'id'=>58,
+            'province_id'=>58,
+            'name_ar'=>'عين قزام',
+            'name_fr'=>'In Guezzam',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ] );
+    }
+}
